25d4665681e282574845e8d191aea34652e52e43
Theeangelgabriel_ <ndlovujnr18@gmail.com>
2025-03-22 22:26:25 +0200

    Add method to add entries to CSV and update initialization logic

--- data_entry.py
+++ data_entry.py
@@ -4,10 +4,27 @@ from datetime import datetime
 
 class CSV:
     CS_FILE = "finance_data.csv"
+    COLUMNS = ["date", "amount", "category", "Description"]
 
     @classmethod
     def initialize_csv(cls):
         try:
             pd.read_csv(cls.CS_FILE)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount","category", "Description"])
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CS_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "Description": description
+        }
+        with open(cls.CS_FILE, "a", newline="") as csfile:
+            writer = csv.IctWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully")
+
+CSV.initialize_csv()
