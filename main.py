b56750ee4e8b1c0133fbdce064a6a3fa2957f668
Theeangelgabriel_ <ndlovujnr18@gmail.com>
2025-03-24 09:01:18 +0200

    Enhance data entry functionality with validation for date, amount, and category inputs; add transaction summary feature

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
 
 class CSV:
-    CS_FILE = "finance_data.csv"
+    CSV_FILE = "finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
         try:
-            pd.read_csv(cls.CS_FILE)
+            pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
             df = pd.DataFrame(columns=cls.COLUMNS)
-            df.to_csv(cls.CS_FILE, index=False)
+            df.to_csv(cls.CSV_FILE, index=False)
 
     @classmethod
     def add_entry(cls, date, amount, category, description):
@@ -22,10 +24,47 @@ class CSV:
             "category": category,
             "description": description
         }
-        with open(cls.CS_FILE, "a", newline="") as csfile:
+        with open(cls.CSV_FILE, "a", newline="") as csfile:
             writer = csv.DictWriter(csfile, fieldnames=cls.COLUMNS)
             writer.writerow(new_entry)
         print("Entry added successfully")
 
-CSV.initialize_csv()
-CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
+    @classmethod
+    def get_transations(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found for the given date range")
+        else:
+            print(f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+
+            print(filtered_df.to_string(index=False, formatters={"date":lambda x: x.strftime(CSV.FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+            print("\nSumary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net: ${(total_income - total_expense):.2f}")
+        
+        return filtered_df
+
+
+
+
+def add():
+    CSV.initialize_csv()
+    date = get_date("Enter the date of the transaction (dd-mm-yyyy) or press Enter for today's date:", allow_default=True )
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+
+
